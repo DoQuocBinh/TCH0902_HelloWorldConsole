fc6beb660a712fc67b2b2f6a9051168c4c70e269
DoQuocBinh <binhdq@fpt.edu.vn>
2021-07-29 16:10:16 +0700

    update

---
+++ HelloWorldConsole/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldConsole
+{
+    partial class Person:IComparable<Person>
+    {
+        public string Address { get; set; }
+
+        public int CompareTo(Person otherPerson)
+        {
+            return Age.CompareTo(otherPerson.Age);
+        }
+    }
+}
--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HelloWorldConsole
 {
-    class Person
+    
+    partial class Person
     {
         public string Name { get; set; }
         public int Age { get; set; }
@@ -18,8 +20,37 @@ namespace HelloWorldConsole
             Console.WriteLine("The value of a is {0} and the value of b is {1}",a,b);
         }
     }
+    public static class MyUtil
+    {
+        public static bool IsGoodEmail(this string email)
+        {
+            if (email.Contains("@"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool isMyyyyyyyyyyyOdd(this int number)
+        {
+            if (number % 2 == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
     class Program
     {
+        //default passing data by value. If you want to get the value out of the function=>get by reference
+        static void GetDataFromInternet(ref int salary) //pass by reference
+                {
+                    salary = 25;//25 from database
+                    Console.WriteLine("Salary is assigned with value: {0}", salary);
+                }
+
+        
         static void Main(string[] args)
         {
             //var d = DateTime.Now.ToString("dd/MM/yyyy hh:mm");
@@ -35,9 +66,48 @@ namespace HelloWorldConsole
             //var infor = p.GetPersonInfo();
             //Console.WriteLine("Name: {0} Age: {1}",infor.PName,infor.PAge);
             p.TestOptions(); // a = K , b = false
-            //p.TestOptions("Tom"); //a = Tom, b = false
-            //p.TestOptions("Jerry", true);//a = Jerry b = true
+                             //p.TestOptions("Tom"); //a = Tom, b = false
+                             //p.TestOptions("Jerry", true);//a = Jerry b = true
 
+            //int mySalary = 0;
+            //GetDataFromInternet(ref mySalary);
+            //Console.WriteLine("My current salary: {0}",mySalary);
+
+            //test property address in partial class
+            var p3 = new Person { Name = "Jimmy", Age = 25, Address = "London" };
+
+            //List<int> supports sort by default
+            List<int> ds = new List<int> { 44,66,22,88,3467,777};
+            ds.Sort();
+            foreach (var item in ds)
+            {
+                Console.WriteLine(item);
+            }
+
+            //List<Person) doesn't support Sort by default
+            List<Person> listPerson = new List<Person>
+            {
+                new Person{Name="Betty",Age=34},
+                new Person{Name="Adam", Age=37},
+                new Person {Name="Zidan",Age=36}
+            };
+            listPerson.Sort();
+            foreach (var item in listPerson)
+            {
+                Console.WriteLine("Name : {0}, Age: {1}",item.Name,item.Age);
+            }
+
+            string email = "billgatesmicrosoft.com";
+            if (email.IsGoodEmail())
+            {
+                Console.WriteLine("Good email");
+            }
+            else
+            {
+                Console.WriteLine("Not a good email");
+            }
+            int k = 46;
+            Console.WriteLine(k.isMyyyyyyyyyyyOdd());
 
         }
     }
